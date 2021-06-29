6bb5d35038cc5d3e55449bc102752ee1baf52799
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-06-29 09:29:30 +0430

    Add files via upload

---
+++ Belal_OOP_16.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ObjectOreiented;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_OOP_16 extends Belal_OOP_14{
+    // Teacher class
+    String faculty;
+    String department;
+    int salary;
+    String[] subject; 
+    
+    void takeExam() {
+        System.out.println("The teacher is taking exam!");
+    }
+    
+    void teach() {
+        System.out.println("The teacher is teaching!");
+    }
+    
+    void getSalary() {
+        System.out.println("The teacher is getting salary");
+    } 
+}
