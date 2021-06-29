e99e6eeb8236b178c927b7abc06e5c6ef9052fea
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-06-29 09:29:49 +0430

    Add files via upload

---
+++ Belal_OOP_17.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ObjectOreiented;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_OOP_17 extends Belal_OOP_14{
+    // Employee Class
+    int salary;
+    String position;
+    
+    void work() {
+        System.out.println("Employee is working!");
+    } 
+    
+    void getSalary() {
+        System.out.println("Employee is getting salary!");
+    }
+}
