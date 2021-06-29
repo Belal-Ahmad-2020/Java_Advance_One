9b2f5bfc63101abfc4bc3a8d81312c991f8b0f34
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-06-29 09:29:08 +0430

    Add files via upload

---
+++ Belal_OOP_15.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ObjectOreiented;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_OOP_15 extends Belal_OOP_14 {
+    // Student class
+    
+    public String faculty;
+    public String department;   
+    public int semster;
+
+    
+    public void study() {
+        System.out.println(name + " is studying!");   
+    }
+    
+    void participateExam() {
+        System.out.println("Student is participating!");   
+    }
+}
