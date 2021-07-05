a212ebb42cb59f6aefe52407247338699baae84e
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-07-05 23:15:46 +0430

    Add files via upload

--- Belal_OOP_1.java
+++ Belal_OOP_1.java
@@ -22,45 +22,15 @@ public class Belal_OOP_1 {
                 method   - function
         
         Object -- an instance of a class
-                Construct                                                  
+                Construct 
+                    
         
-        to create an object 
-        ClassNmae objectName = new ClassName();
-//        
+        
+        
+        
+        
+        
         */
-        
-        // create an object
-        MotorCycle motor1 = new MotorCycle();
-        
-        motor1.color = "White";
-        motor1.company = "Zarang";
-        motor1.model = "2003";
-        motor1.speed = 170;
-        motor1.tires = 2;
-        motor1.weight = 130;
-        
-        // create another object
-        MotorCycle motor2 = new MotorCycle();
-        
-        motor2.color = "Black";
-        motor2.company = "Wahab";
-        motor2.model = "2007";
-        motor2.speed = 169;
-        motor2.tires = 2;
-        motor2.weight = 170;
-        
-        // Motor 1
-        System.out.println(motor1.color);
-        System.out.println(motor1.company);
-        System.out.println(motor1.speed);
-        
-        
-        // Motor 2
-        System.out.println(motor2.model);
-        System.out.println(motor2.tires);
-        System.out.println(motor2.weight);
-        
-        motor2.doBreak();
     }
 }
 
@@ -73,19 +43,8 @@ class MotorCycle {
     int weight;
     
     void doBreak() {
-        System.out.println("The motorcycle breaked!");
-    }
-    
-    void move() {
-        System.out.println("The motorcycle moving!");
-    }
-    
-    void start() {
-        System.out.println("The motorcycle is started!");
-    }
-    
-    void stop() {
-        System.out.println("The motorcycle has been turned off!");
+        
+        
     }
     
     
